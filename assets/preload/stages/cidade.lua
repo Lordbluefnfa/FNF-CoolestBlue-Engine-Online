24097998933dbed84110d1795fd5f633f47559d1
Lordbluefnfa <107429343+Lordbluefnfa@users.noreply.github.com>
2023-07-31 18:05:41 -0300

    city coding

---
+++ assets/preload/stages/cidade.lua
@@ -0,0 +1,6 @@
+function onCreate()
+	makeLuaSprite('picobg', 'cidade/subway', 650, 600);
+	setScrollFactor('picobg', 0.9, 0.9);
+	scaleObject('picobg', 1.1, 1.1);
+	addLuaSprite('picobg', false);
+	end
